39ef544a3ccf0391ca65a5980c35103b9e238d37
Giovanni Perillo <gigperillo@gmail.com>
2017-08-15 20:56:10 -0300

    Almost there
    The game is almost finished.

--- js/game_functions.js
+++ js/game_functions.js
@@ -1,6 +1,6 @@
 if(localStorage.gameEvent == undefined) localStorage.gameEvent = JSON.stringify({eventos:[]});
 
-function jogoKeyPress(e)
+function addEvento(event, parametro)
 {
     var param = localStorage.getItem('gameEvent');
     var eventos = JSON.parse(param);
@@ -9,41 +9,36 @@ function jogoKeyPress(e)
     //console.log(param);
     //console.log(eventos);
 
-    eventos.push({evento:"press", parametros:e.keyCode});
+    eventos.push({evento:event, parametros:parametro});
 
     //console.log(eventos);
 
-    localStorage.gameEvent = JSON.stringify({eventos});
+    localStorage.gameEvent = JSON.stringify({eventos});   
+}
+
+function getParameterByName(name, url) {
+    if (!url) {
+    url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function jogoKeyPress(e)
+{
+    addEvento("press", e.keyCode);
 }
 
 function jogoKeyHold(e)
 {
-    var param = localStorage.getItem('gameEvent');
-    var eventos = JSON.parse(param);
-    eventos = eventos.eventos; //gambiarra
-
-    //console.log(param);
-    //console.log(eventos);
-
-    eventos.push({evento:"hold", parametros:e.keyCode});
-
-    //console.log(eventos);
-
-    localStorage.gameEvent = JSON.stringify({eventos});
+    addEvento("hold", e.keyCode);
 }
 
 function jogoKeyRelease(e)
 {
-    var param = localStorage.getItem('gameEvent');
-    var eventos = JSON.parse(param);
-    eventos = eventos.eventos; //gambiarra
-
-    //console.log(param);
-    //console.log(eventos);
-
-    eventos.push({evento:"release", parametros:e.keyCode});
-
-    //console.log(eventos);
-
-    localStorage.gameEvent = JSON.stringify({eventos});
+    addEvento("release", e.keyCode);
 }
